42185edf15d23073da05b5d002ddf6c2efb70d96
Hao-LinhDai-2026 <dangxuantrinh1990kg@gmail.com>
2026-04-15 21:22:42 +0700

    Create MainActivity.java

---
+++ ​app/src/main/java/com/tts/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tts.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://google.com");
+    }
+}
